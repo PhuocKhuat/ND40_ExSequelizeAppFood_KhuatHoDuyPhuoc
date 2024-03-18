17178f056c13fd0a6e0163a1f7015cc908926b3e
Phuoc <khuathoduyphuoc2001@gmail.com>
2024-03-18 18:23:48 +0700

    format lại cho getLikeListByRes và likeUnlike (chưa xong)

--- src/controllers/likeController.js
+++ src/controllers/likeController.js
@@ -5,56 +5,87 @@ import initModels from "../models/init-models.js";
 const initModel = initModels(sequelizeConnect);
 const likeUnlike = async (req, res) => {
   try {
-    const { user_id, res_id } = req.body;
-    const existingLike = await initModel.like_res.findOne({
-      where: {
-        user_id: user_id,
-        res_id: res_id,
-      },
-    });
-    if (existingLike) {
-      if (existingLike.isLike == 1) {
-        await existingLike.update({ date_like: new Date(), isLike: 0 });
-      } else{
-        await existingLike.update({ date_like: new Date(), isLike: 1 });
-        // await existingLike.destroy();
-      }
-    } else {
-      await initModel.like_res.create({
-        user_id: user_id,
-        res_id: res_id,
-        date_like: new Date(),
-        isLike: 1,
+    try {
+      const { user_id, res_id } = req.body;
+      const existingLike = await initModel.like_res.findOne({
+        where: {
+          user_id: user_id,
+          res_id: res_id,
+        },
+        include: [
+          {
+            model: initModel.users,
+            as: "user",
+            attributes: ["full_name", "email", "password"],
+          },
+          {
+            model: initModel.restaurants,
+            as: "re",
+            attributes: ["res_name", "image", "description"],
+          },
+        ],
       });
+      if (existingLike) {
+        if (existingLike.isLike == 1) {
+          await existingLike.update({ date_like: new Date(), isLike: 0 });
+        } else {
+          await existingLike.update({ date_like: new Date(), isLike: 1 });
+          // await existingLike.destroy();
+        }
+      } else {
+        await initModel.like_res.create({
+          user_id: user_id,
+          res_id: res_id,
+          date_like: new Date(),
+          isLike: 1,
+        });
+      }
+      return responseData(res, 200, "Processed successfully", existingLike);
+    } catch (error) {
+      return responseData(res, 400, "Not Found");
     }
-    return responseData(res, 200, "Processed successfully");
   } catch (error) {
     return responseData(res, 500, "Error processing request");
   }
 };
 
 const getLikeListByRes = async (req, res) => {
-  const { resId } = req.params;
-  const content = await initModel.like_res.findAll({
-    attributes: ["res_id", "user_id", "date_like"],
-    include: [
-      {
-        model: initModel.restaurants,
-        as: "re",
-        attributes: ["res_name", "image", "description"],
+  try {
+    const { resId } = req.params;
+    const existingLike = await initModel.like_res.findOne({
+      where: {
+        res_id: resId,
       },
-      {
-        model: initModel.users,
-        as: "user",
-        attributes: ["full_name", "email", "password"],
+    });
+    if (!existingLike) {
+      return responseData(res, 400, "Not found res Id");
+    }
+    const likeList = await initModel.like_res.findAll({
+      include: ["user", "re"],
+      where: {
+        res_id: resId,
       },
-    ],
-    where: {
-      res_id: resId,
-    },
-    raw: true,
-  });
-  responseData(res, 200, "Processed successfully", { content });
+    });
+    const formattedLikes = likeList.map((like) => ({
+      resId: like.res_id,
+      dateLike: like.date_like,
+      isLiked: like.isLike,
+      res: {
+        resName: like.re.res_name,
+        image: like.re.image,
+        description: like.re.description,
+      },
+      user: {
+        userId: like.user.user_id,
+        fullName: like.user.full_name,
+        email: like.user.email,
+        password: like.user.password,
+      },
+    }));
+    responseData(res, 200, "Processed successfully", formattedLikes);
+  } catch (error) {
+    return responseData(res, 500, "Error processing request");
+  }
 };
 
 const getLikeListByUser = async (req, res) => {
