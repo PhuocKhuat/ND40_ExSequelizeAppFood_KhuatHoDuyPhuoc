d93acf4bd5a8811a0a6a6d59d7cf243443df512d
Phuoc <khuathoduyphuoc2001@gmail.com>
2024-03-24 23:28:51 +0700

    Làm lại like-unlike (logic, formatCode)

--- src/configs/response.js
+++ src/configs/response.js
@@ -1,9 +1,9 @@
-const responseData = (res, status,message,  content)=>{
+const responseData = (res, status, message, content) => {
   res.status(status).json({
     status,
     message,
     content,
     date: new Date(),
-});
+  });
 };
 export default responseData;
--- src/controllers/likeController.js
+++ src/controllers/likeController.js
@@ -5,45 +5,94 @@ import initModels from "../models/init-models.js";
 const initModel = initModels(sequelizeConnect);
 const likeUnlike = async (req, res) => {
   try {
-    try {
-      const { userId, resId } = req.body;
-      const existingLike = await initModel.like_res.findOne({
-        where: {
-          user_id: userId,
-          res_id: resId,
-        },
-        include: [
-          {
-            model: initModel.users,
-            as: "user",
-            attributes: ["full_name", "email", "password"],
-          },
-          {
-            model: initModel.restaurants,
-            as: "re",
-            attributes: ["res_name", "image", "description"],
-          },
-        ],
-      });
-      if (existingLike) {
-        if (existingLike.is_like == 1) {
-          await existingLike.update({ date_like: new Date(), is_like: 0 });
-        } else {
-          await existingLike.update({ date_like: new Date(), is_like: 1 });
-          // await existingLike.destroy();
-        }
-      } else {
-        await initModel.like_res.create({
-          user_id: userId,
-          res_id: resId,
-          date_like: new Date(),
-          is_like: 1,
-        });
-      }
-      return responseData(res, 200, "Processed successfully", existingLike);
-    } catch (error) {
-      return responseData(res, 400, "Not found user id or res id");
+    const { userId, resId } = req.body;
+
+    const checkUser = await initModel.users.findOne({
+      where: {
+        user_id: userId,
+      },
+    });
+
+    const checkRes = await initModel.restaurants.findOne({
+      where: {
+        res_id: resId,
+      },
+    });
+
+    const existingLike = await initModel.like_res.findOne({
+      where: {
+        user_id: userId,
+        res_id: resId,
+      },
+      include: ["user", "re"],
+    });
+
+    if (!checkUser) {
+      return responseData(res, 404, "Not found user id");
     }
+    if (!checkRes) {
+      return responseData(res, 404, "Not found res id");
+    }
+
+    let formattedLikes = {};
+
+    if (existingLike) {
+      if (existingLike.is_like == 1) {
+        await existingLike.update({ date_like: new Date(), is_like: 0 });
+      } else {
+        await existingLike.update({ date_like: new Date(), is_like: 1 });
+        // await existingLike.destroy();
+      }
+      formattedLikes = {
+        likeResId: existingLike.like_res_id,
+        isLiked: existingLike.is_like,
+        dateLike: existingLike.date_like,
+        user: {
+          userId: existingLike.user.user_id,
+          fullName: existingLike.user.full_name,
+          email: existingLike.user.email,
+          password: existingLike.user.password,
+        },
+        res: {
+          resId: existingLike.re.user_id,
+          resName: existingLike.re.res_name,
+          image: existingLike.re.image,
+          description: existingLike.re.description,
+        },
+      };
+    } else {
+      const newLike = await initModel.like_res.create({
+        user_id: userId,
+        res_id: resId,
+        date_like: new Date(),
+        is_like: 1,
+      });
+      const relationship = await initModel.like_res.findOne({
+        where: {
+          like_res_id: newLike.like_res_id,
+        },
+        include: ["user", "re"],
+      });
+      formattedLikes = {
+        likeResId: relationship.like_res_id,
+        isLiked: relationship.is_like,
+        dateLike: relationship.date_like,
+        user: {
+          userId: relationship.user.user_id,
+          fullName: relationship.user.full_name,
+          email: relationship.user.email,
+          password: relationship.user.password,
+        },
+        res: {
+          resId: relationship.re.user_id,
+          resName: relationship.re.res_name,
+          image: relationship.re.image,
+          description: relationship.re.description,
+        },
+      };
+    }
+
+    return responseData(res, 200, "Processed successfully", formattedLikes);
   } catch (error) {
     return responseData(res, 500, "Error processing request");
   }
@@ -92,10 +141,10 @@ const getLikeListByUser = async (req, res) => {
   try {
     const { userId } = req.params;
     const existingLike = await initModel.like_res.findOne({
-      where:{
+      where: {
         user_id: userId,
-      }
-    })
+      },
+    });
     if (!existingLike) {
       return responseData(res, 400, "Not found user id");
     }
